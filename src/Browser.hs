e37b9c13752e2672658f640abca62d07cc8550c8
Mark Carter <mcturra2000@yahoo.co.uk>
2015-07-28 15:05:44 +0100

    Added Linux browser to sifi

--- src/Browser.hs
+++ src/Browser.hs
@@ -6,21 +6,25 @@ import System.Process
 import Utils
 
 -- url is of form: "file:///C:/Users/mcarter/AppData/Local/MarkCarter/sifi/sifi.htm"
+rawUrl = do
+  url1 <- outFile "sifi.htm"
+  return  ("file:///" ++ url1)
+
 
 openLinuxBrowser = do
+  url <- rawUrl
+  --ret <- rawSystem "firefox" [url]
+  ret <- spawnProcess "firefox" [url]
   return ()
 
 openWinBrowser = do
-   url1 <- outFile "sifi.htm"
+   url1 <- rawUrl -- outFile "sifi.htm"
    let url2 = map (\c -> if c == '\\' then '/' else c) url1
-   let url3 = "file:///" ++ url2
-   --print url3
-   ret <- rawSystem "explorer" [url3]
+   --let url3 = "file:///" ++ url2
+   --print url2
+   ret <- rawSystem "explorer" [url2]
    return ()
-   --putStrLn url3
-  --ret <- rawSystem "explorer" ["file:///C:/Users/mcarter/AppData/Local/MarkCarter/sifi/sifi.htm"]
-  --print ret
-  --putStrLn "Done" 
+
 
 openBrowser :: IO ()
 openBrowser = if isLinux then openLinuxBrowser else openWinBrowser
