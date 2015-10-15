b1e74dcc61ede6f1e7989eb0a5944c0b58745de0
unknown <mcturra2000@yahoo.co.uk>
2015-10-15 12:27:28 +0100

    Refactored Returns.hs

--- src/Returns.hs
+++ src/Returns.hs
@@ -13,29 +13,34 @@ fmtReturn :: Return -> String
 fmtReturn ret =
   printf fmt aIdx aDstamp aMine minepc aAsx asxpc out
   where
-    fmt = "%3d %11s %6.2f %6.2f %4.0f %6.2f %6.2f"
+    fmt = "%3d %10s %6.2f %6.2f %4.0f %6.2f %6.2f"
     Return aIdx aDstamp aMine minepc aAsx asxpc out = ret
 
+summaryLine :: Return -> String
+summaryLine r =
+  let fmt = "%3s %-10s %6s %6.2f %4s %6.2f %6.2f" in
+  printf  fmt "" "AVG" "" (rtMinepc r) "" (rtAsxpc r) (rtOutpc r)
 
-  
+
+
 deltaReturns r prev  =
-  Return (rtIdx r) (rtDstamp r) (rtMine r) minepc (rtAsx r) asxpc out
+  r { rtMinepc = minepc , rtAsxpc = asxpc, rtOutpc = outpc }
   where
-    minepc = gainpc (rtMine r) (rtMine prev)
-    asxpc  = gainpc (rtAsx  r) (rtAsx  prev)
-    out    = minepc - asxpc
- 
+    gap = (fromIntegral $ (rtIdx r) - (rtIdx prev))::Double
+    gpc func =  100.0* ((func r) / (func prev)) ** (1.0/gap) - 100.0
+    minepc = gpc rtMine
+    asxpc = gpc rtAsx
+    outpc    = minepc - asxpc
 
-summaryLine :: Double -> Double -> Double -> String
-summaryLine minepa asxpa outpa =
-  printf "%15s %6s %6.2f %4s %6.2f %6.2f\n" "AVG" " " minepa " " asxpa outpa
+
+
 
 createReturns :: Dstamp -> [Etran] -> Double -> [Return] -> [String]
 createReturns _ _ _ [] = [] -- in case no returns have been specified
 createReturns ds etrans asxNow returns =
   [hdr] ++ createdReturns ++ [summary]
   where
-    hdr = "IDX      DSTAMP   MINE  MINE%  ASX   ASX%   OUT%"
+    hdr = "IDX     DSTAMP   MINE  MINE%  ASX   ASX%   OUT%"
     ret0 = head returns
     lastRet = last returns
     finIdx = 1 + (rtIdx lastRet)
@@ -46,23 +51,7 @@ createReturns ds etrans asxNow returns =
     finRet = Return finIdx ds finMine 0 asxNow 0 0
 
     augReturns = returns ++ [finRet] 
-    --createdReturns = foldReturns [] ret0 augReturns
     createdReturns1 = accum deltaReturns  ret0 augReturns
     createdReturns = map fmtReturn createdReturns1
-    
-
-    -- summary line
-    gpc v =
-      gainpc power 1.0
-      where
-        fix = (fromIntegral finIdx)::Double
-        inv = 1.0/ fix
-        power =  v ** inv
-        
-    minepa = gpc (finMine/100.0)
-    asx0 = rtAsx ret0
-    asxpa  = gpc (asxNow/asx0)
-    outpa = gpc (finMine *asx0 / asxNow / 100.0)
-    summary = summaryLine minepa asxpa outpa
-
-
+ 
+    summary = summaryLine $ deltaReturns finRet ret0
