72fdfe25a7726318b19a1ccf59a1d52706b8be9a
unknown <mcturra2000@yahoo.co.uk>
2015-10-16 14:18:08 +0100

    Messing around with Scratch

--- Scratch.hs
+++ Scratch.hs
@@ -1,7 +1,30 @@
-{-# LANGUAGE TemplateHaskell #-}
+--{-# LANGUAGE TemplateHaskell #-}
 
-import Utils
+--import Debug.Trace
+--{-# OPTIONS_GHC -prof #-}
 
-x = []
-v1 = $(headQ 'x)
---v2 = headQ x
+
+data Caller a = Result a | Message String | Fail deriving (Show)
+
+
+instance Functor Caller where
+  fmap f (Result x) = Result (f x)
+  fmap _ (Message x) = Message x
+  fmap _ Fail = Fail
+
+instance Applicative Caller where
+  pure = Result
+  (Result f) <*> (Result x) = Result (f x)
+  
+
+{-
+instance Functor (Caller a) where
+  fmap _ (Msg x) 
+-}
+
+{-
+instance Monad Caller where
+  return x = Caller "" x
+  Msg x >>= _ = Msg x
+  Call x >>= _ = Call x
+-}
