63a2258da7bd063f0e6ec0a192a9856be69eba5a
unknown <mcturra2000@yahoo.co.uk>
2015-11-10 09:07:02 +0000

    Refactor Post.hs

--- src/Post.hs
+++ src/Post.hs
@@ -4,31 +4,21 @@ import Data.List
 import Data.Maybe
 import Data.Ord
 import Text.Printf
-import Data.Tuple.Select
 
 import Aggregate
 import Etran
 import Ledger
---import Ntran
 import Types
 import Utils
 
--- FIXME EASY to more conventional form
-data Post = Post Dstamp Acc Acc Pennies Desc deriving (Show)
+data Post = Post {
+  postDstamp::Dstamp
+  , postDr::Acc
+  , postCr::Acc
+  , postPennies::Pennies
+  , postDec::Desc}
+          deriving (Show)
 
-postTuple (Post dstamp dr cr pennies desc) =
-  (dstamp, dr, cr, pennies, desc)
-
-
-postDstamp p = sel1 $ postTuple p
-
-postDr :: Post -> Acc
-postDr p = sel2 $ postTuple p
-
-postCr :: Post -> Acc
-postCr p = sel3 $ postTuple p
-postPennies p = sel4 $ postTuple p
-postDesc p = sel5 $ postTuple p
 
 postingsFromNtran ntran =
   [n1, n2]
@@ -39,16 +29,12 @@ postingsFromNtran ntran =
     
 postingsFromNtrans  ntrans = concatMap postingsFromNtran ntrans
 
---postingsFromEtran e = []
 
 postingsFromEtran :: Etran -> [Post]
 postingsFromEtran e =
   posts
   where
-    --cost = etAmount e
     folio = etFolio e
-    --de = etranDerived e
-    --(odstamp, startValue, flow, profit, endValue, comm) = de
     ds = etDstamp e
     s = etSym e
     pCost = Post ds folio "pCost" (negp $ etAmount e) ("pCost:" ++ s) -- actual cost
@@ -56,11 +42,9 @@ postingsFromEtran e =
     pVcd  = Post ds (folio ++ "/c") "pVcd" ( etVcd e) ("pVcd:" ++ s) -- value c/d
     posts1 = [pCost, pPdp, pVcd]
 
-    --pPbd1 = Post ds (folio ++ "/b") "pPdb1" (negp $ etVbd e) ("pPdb1:" ++ s)
     pPbd2 = Post ds "opn" "pdb2" (negp $ etPbd e) ("pdb2:" ++ s) -- profit b/d
 
     posts = if etBetween e then posts1 else pPbd2:posts1
-    --posts = posts1
     
 
 
@@ -79,15 +63,14 @@ createPostings ntrans derivedEtrans =
   postings
   where
     ntranPostings = postingsFromNtrans ntrans
-    --derivedEtrans = deriveEtrans start comms etrans
     etranPostings = postingsFromEtrans derivedEtrans
     unsortedPostings = ntranPostings ++ etranPostings
     postings = sortBy (comparing $ postDstamp) unsortedPostings
     
 
---showPost :: Post -> String
+showPost :: Post -> String
 showPost p =
-  let (dstamp, dr, cr, pennies, desc) = postTuple p in
+  let Post dstamp dr cr pennies desc = p in
   printf "%s %4.4s %4.4s %20.20s %s" dstamp dr cr desc (show pennies) 
 
 -- | Aggregate posts into account and date order
